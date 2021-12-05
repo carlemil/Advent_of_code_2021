89fecabea9657e91f87d0f523f013b422ad6208f
Carl-Emil Kjellstrand <erbnsman@gmail.com>
2021-12-05 13:18:31 +0100

    Day 2 b completed.

--- src/main/kotlin/Day02.kt
+++ src/main/kotlin/Day02.kt
@@ -5,9 +5,8 @@ fun main() {
     val inputStream: InputStream = File("src/main/resources/data_day02.txt").inputStream()
     val data = mutableListOf<String>()
     inputStream.bufferedReader().forEachLine { data.add(it) }
-    println("Day 2")
     solve2A(data)
-    //solve2B(data)
+    solve2B(data)
 }
 
 fun solve2A(data: MutableList<String>) {
@@ -24,5 +23,14 @@ fun solve2A(data: MutableList<String>) {
 }
 
 fun solve2B(data: MutableList<String>) {
-
+    val (a, b) =
+        data.map { p -> p.split(" ") }
+            .fold(Triple(0, 0, 0)) { p, t ->
+                when (t[0]) {
+                    "down" -> Triple(p.first, p.second, p.third + t[1].toInt())
+                    "up" -> Triple(p.first, p.second, p.third - t[1].toInt())
+                    else -> Triple(t[1].toInt() + p.first, p.second + p.third * t[1].toInt(), p.third)
+                }
+            }
+    println(a * b)
 }
