7c2fdbff9920716bf6337668a2e692da915338d9
Carl-Emil Kjellstrand <erbnsman@gmail.com>
2021-12-06 23:39:51 +0100

    Day 4.1 done.

--- src/main/kotlin/Day04.kt
+++ src/main/kotlin/Day04.kt
@@ -5,7 +5,7 @@ fun main() {
     val inputStream: InputStream = File("src/main/resources/data_day04.txt").inputStream()
     val data = mutableListOf<String>()
     inputStream.bufferedReader().forEachLine { data.add(it) }
-    val numbers = data.first().split(",")
+    val numbers = data.first().split(",").map { it.toInt() }
     val boards = data.drop(1) // Drop the numbers
         .windowed(size = 6, step = 6) // Cut out the boards
         .map { board -> // Map the boards
@@ -15,15 +15,57 @@ fun main() {
                         .map { it.trim().toInt() } // Map each number to an int
                 }
         }
-    solve4A(numbers, boards)
-    solve4B(data)
+    val markBoards = MutableList(boards.size) { MutableList(5) { MutableList(5) { 0 } } }
+
+    solve4A(numbers, boards, markBoards)
+    solve4B(numbers, boards)
 }
 
-fun solve4A(numbers: List<String>, data: List<List<List<Int>>>) {
-
-    print("")
+fun solve4A(numbers: List<Int>, boards: List<List<List<Int>>>, mbs: MutableList<MutableList<MutableList<Int>>>) {
+    numbers.forEach { n ->
+        boards.forEachIndexed { i, _ ->
+            markInBoard(n, boards[i], mbs[i])
+        }
+    }
+    print(mbs)
 }
 
-fun solve4B(data: MutableList<String>) {
+fun markInBoard(n: Int, b: List<List<Int>>, mb: MutableList<MutableList<Int>>) {
+    for (i in 0..4) {
+        for (j in 0..4) {
+            if (b[i][j] == n) {
+                mb[i][j] = 1
+                if (checkWinningCondition(mb, i, j)) {
+                    printScore(b, mb, n)
+                }
+            }
+        }
+    }
+}
+
+fun printScore(b: List<List<Int>>, mb: MutableList<MutableList<Int>>, n: Int) {
+    var score = 0
+
+    for (i in 0..4) {
+        for (j in 0..4) {
+            if (mb[i][j] == 0) {
+                score += b[i][j]
+            }
+        }
+    }
+    score *= n
+
+    println(score)
+
+    System.exit(0)
+}
+
+fun checkWinningCondition(mb: MutableList<MutableList<Int>>, i: Int, j: Int): Boolean {
+    return ((mb[0][j] == 1) and (mb[1][j] == 1) and (mb[2][j] == 1) and (mb[3][j] == 1) and (mb[4][j] == 1)) ||
+            ((mb[i][0] == 1) and (mb[i][1] == 1) and (mb[i][2] == 1) and (mb[i][3] == 1) and (mb[i][4] == 1))
+
+}
+
+fun solve4B(numbers: List<Int>, data: List<List<List<Int>>>) {
 
 }
