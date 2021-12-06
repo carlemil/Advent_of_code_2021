f82e1979a07265073b067625959e31e44c0103f2
Carl-Emil Kjellstrand <erbnsman@gmail.com>
2021-12-06 09:21:46 +0100

    day 4.1, reading and parsing data.

--- src/main/kotlin/Day04.kt
+++ src/main/kotlin/Day04.kt
@@ -5,11 +5,21 @@ fun main() {
     val inputStream: InputStream = File("src/main/resources/data_day04.txt").inputStream()
     val data = mutableListOf<String>()
     inputStream.bufferedReader().forEachLine { data.add(it) }
-    solve4A(data)
+    val numbers = data.first().split(",")
+    val boards = data.drop(1) // Drop the numbers
+        .windowed(size = 6, step = 6) // Cut out the boards
+        .map { board -> // Map the boards
+            board.drop(1) //  Drop first row, its empty
+                .map { boardRow -> // Map rows in each board
+                    boardRow.trim().split(Regex("\\s+")) // Split out numbers from each row
+                        .map { it.trim().toInt() } // Map each number to an int
+                }
+        }
+    solve4A(numbers, boards)
     solve4B(data)
 }
 
-fun solve4A(data: MutableList<String>) {
+fun solve4A(numbers: List<String>, data: List<List<List<Int>>>) {
 
     print("")
 }
